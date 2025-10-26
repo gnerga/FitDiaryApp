bc44ca6da402e105846b7c11d3f9fa23379c52ec
Grzegorz Nerga <gregnerga@gmail.com>
2025-10-27 00:20:40 +0100

    Task-1: Add MealType enum

---
+++ MealType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitDiaryApp
+{
+    public enum MealType
+    {
+        BREAKFAST = 1,
+        BRUNCH = 2,
+        LUNCH = 3, 
+        SNACK = 4,
+        DINNER = 5
+    }
+}
